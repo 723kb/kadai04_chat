5fbe73196077a49aece7b38854353e826178a91a
723kb <pp5_vb.swd723@icloud.com>
2024-06-05 01:35:43 +0900

    css修正,フィルタリング入力クリアボタン設置

--- js/app.js
+++ js/app.js
@@ -31,15 +31,15 @@ $(document).ready(() => {
   }
 
   // フィルタリング機能説明のtoggleメソッド
-  $("#filter").hide();
+  $("#filterArea").hide();
   $("#search").on("click", () => {
-    $("#filter").slideToggle();
+    $("#filterArea").slideToggle();
   });
 });
 
 // メモをリストに追加する関数
 // isJapanese 日本語のメモかどうかを判定するためのパラメーター名
-const addMemoToList = (list, title, text, isJapanese, timestamp) => {
+const addMemoToList = (list, text, isJapanese, timestamp) => {
   const li = $('<li></li>').addClass('w-full flex mb-4');
   // const timestamp = Date.now(); // メッセージを判別するために、現在のタイムスタンプを取得
   // タイムスタンプを使用して日時をフォーマット
@@ -54,24 +54,25 @@ const addMemoToList = (list, title, text, isJapanese, timestamp) => {
   if (isJapanese) { // isJapaneseがtrueなら、日本語のメモを追加
     li.html(`
       <div class="chat-bubble right" data-timestamp="${timestamp}"> 
-        <h3>${title}</h3>
-        <p>日本語: <span class='jp-text'>${text}</span></p>
-        <p>${formattedDate}</p>
+        <h3>日本語 : </h3>
+        <p class='jp-text'>${text}</p>
+        <p class="text-end">${formattedDate}</p>
       </div>
     `);
   } else { // isJapaneseがfalseなら、中国語のメモを追加
     li.html(`
       <div class="chat-bubble left " data-timestamp="${timestamp}">
-        <h3>${title}</h3>
-        <p>中国語: <span class='cn-text'>${text}</span></p>
-        <p>${formattedDate}</p>
+        <h3>中国語 : </h3>
+        <p class='cn-text'>${text}</p>
+        <p class="text-end">${formattedDate}</p>
       </div>
     `);
   }
 
   list.append(li);
 
-  // 追加した投稿の位置までページをスクロールする
+  // addMemo関数を実行した時のみ追加した投稿の位置までページをスクロールする
+  
   const scrollTo = li.offset().top;
   $('html, body').animate({
     scrollTop: scrollTo
@@ -94,28 +95,25 @@ const translateText = async (text) => {
 // メモを保存する関数
 const saveMemo = async () => {
   // console.log('saveMemo 関数が呼び出されました');
-  const title = $('#title').val();
   const text = $('#text').val(); // タイトルと本文を取得
 
-  if (title && text) { // タイトルと本文が入力されている場合のみ処理を行う
+  if (text) { // タイトルと本文が入力されている場合のみ処理を行う
     try {
       const translatedText = await translateText(text); // テキストを翻訳
       const timestamp = Date.now(); // 現在のタイムスタンプを取得
       const newMemoRef = push(dbRef); // データベースの新しい参照を作成
       set(newMemoRef, { // タイトル、本文、翻訳済みテキスト、タイムスタンプをデータベースに保存
-        title: title,
         text: text,
         translatedText: translatedText,
         timestamp: timestamp
       });
 
       // 日本語のメッセージをリストに追加し、即時表示
-      addMemoToList($('#list'), title, text, true, timestamp); // addMemoToListに渡されるisJapaneseがtrueの場合
+      addMemoToList($('#list'), text, true, timestamp); // addMemoToListに渡されるisJapaneseがtrueの場合
       // 一定時間後に中国語のメッセージを表示
       setTimeout(() => {
-        addMemoToList($('#list'), title, translatedText, false, timestamp); // addMemoToListに渡されるisJapaneseがfalseの場合
+        addMemoToList($('#list'), translatedText, false, timestamp); // addMemoToListに渡されるisJapaneseがfalseの場合
       }, 2000); // 2000ミリ秒（2秒）後に表示
-      $('#title').val('');
       $('#text').val('');
     } catch (error) {
       console.error('Error translating text:', error);
@@ -155,8 +153,8 @@ const loadMemos = () => {
     messages.sort((a, b) => a.timestamp - b.timestamp); // 比較関数 timestampが小さい(古い)順に並び替え
 
     messages.forEach((message) => { // 各メッセージに対して以下実行
-      addMemoToList($('#list'), message.title, message.text, true, message.timestamp); // 日本語のメッセージをリストに追加
-      addMemoToList($('#list'), message.title, message.translatedText, false, message.timestamp); // 中国語のメッセージをリストに追加
+      addMemoToList($('#list'), message.text, true, message.timestamp); // 日本語のメッセージをリストに追加
+      addMemoToList($('#list'), message.translatedText, false, message.timestamp); // 中国語のメッセージをリストに追加
     });
   }).catch(error => {
     console.error('データベースからメッセージを取得中にエラーが発生しました:', error);
@@ -171,8 +169,8 @@ $('#filter').on('input', () => {
     snapshot.forEach((childSnapshot) => { // 取得したテータをここのメモに分割して処理
       const childData = childSnapshot.val();
       if (childData.text.toLowerCase().includes(filter)) { // タイトルが入力された値を含む場合のみ以下の処理
-        addMemoToList($('#list'), childData.title, childData.text, true, childData.timestamp); // 日本語メッセージ
-        addMemoToList($('#list'), childData.title, childData.translatedText, false, childData.timestamp); // 中国語メッセージ
+        addMemoToList($('#list'), childData.text, true, childData.timestamp); // 日本語メッセージ
+        addMemoToList($('#list'), childData.translatedText, false, childData.timestamp); // 中国語メッセージ
       }
     });
   });
